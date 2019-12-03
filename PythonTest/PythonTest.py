6b2274329af40fea15ffb6f079812c5bc23d70b2
dacelectronicssa@gmail.com <dacelectronicssa@gmail.com>
2019-12-03 19:03:58 +0200

    Create Sqlite db Get location and save location to db

--- PythonTest/PythonTest.py
+++ PythonTest/PythonTest.py
@@ -3,16 +3,82 @@ from telepot.loop import MessageLoop
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 from pprint import pprint
 #import ConfigParser
-#import threading
-#from threading import Thread
-#import time
-#import datetime
+import threading
+from threading import Thread
+import time
+import datetime
 from datetime import datetime
 from datetime import timedelta
+import sqlite3
+from sqlite3 import Error
 
 bot = telepot.Bot('320663225:AAFVzc1y_7dLUu97g1kqbw9PxkQU1aiSUMk')
+SqlitePath = 'D:\pythonsqlite.db'
+
 
 WelcomeMessage = "Welcome to <Placeholer>, please send us your the name of your suburb or hit the GEO LOCATION button below"
+
+sql_create_user_table = """CREATE TABLE IF NOT EXISTS tasks (
+                                    id integer PRIMARY KEY,
+                                    user_id integer NOT NULL,
+                                    name text NOT NULL,
+                                    latitude text NOT NULL,
+                                    longitude text NOT NULL
+                                );"""
+
+def create_intitaldb(conn, task):
+    """
+    Create a new task
+    :param conn:
+    :param task:
+    :return:
+    """
+ 
+    sql = ''' INSERT INTO tasks(user_id,name,latitude,longitude)
+              VALUES(?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, task)
+    return cur.lastrowid
+
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print("Sqlite db Opened v" + sqlite3.version)
+        return conn
+    except Error as e:
+        print(e)
+ 
+    return conn
+
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+
+def select_all_tasks(conn):
+    """
+    Query all rows in the tasks table
+    :param conn: the Connection object
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM tasks")
+ 
+    rows = cur.fetchall()
+ 
+    for row in rows:
+        print(row)
 
 
 def handle(msg):
@@ -24,7 +90,15 @@ def handle(msg):
     
     if "location" in msg:
         print ("Location Found")
-    
+        SqliteInitialinfo = (msg["from"]["id"], msg["from"]["first_name"], msg["location"]["latitude"], msg["location"]["longitude"])
+        print ("Saving To Sqlite Database")
+        conn = create_connection(SqlitePath)
+        create_intitaldb(conn, SqliteInitialinfo)
+        print ("Saved To Sqlite Database")
+        select_all_tasks(conn)
+        conn.close()
+
+        
     
     if msg['text'] == "/start":
         bot.sendMessage(chat_id, WelcomeMessage,
@@ -34,7 +108,27 @@ def handle(msg):
                                 ]
                             ))
         
- 
+
+
+
+
+
+
+print ("Setting Up Sqlite Database")
+conn = create_connection(SqlitePath)
+            
+if conn is not None:
+        # create user database table
+        create_table(conn, sql_create_user_table)
+        print("Sqlite db Tables Created")
+        conn.close()
+else:
+        print("Error! cannot create the database connection.")
+
+
+
+
+
 MessageLoop(bot, handle).run_as_thread()
 
 ReplyKeyboardMarkup(keyboard=[[
