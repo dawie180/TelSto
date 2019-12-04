d36bb9b8c990acb28698c7a5cb1763cdb30e9ef8
dacelectronicssa@gmail.com <dacelectronicssa@gmail.com>
2019-12-04 16:53:48 +0200

    keyerror handling

--- PythonTest/PythonTest.py
+++ PythonTest/PythonTest.py
@@ -16,9 +16,9 @@ bot = telepot.Bot('320663225:AAFVzc1y_7dLUu97g1kqbw9PxkQU1aiSUMk')
 SqlitePath = 'D:\pythonsqlite.db'
 
 
-WelcomeMessage = "Welcome to <Placeholer>, please send us your the name of your suburb or hit the GEO LOCATION button below"
+WelcomeMessage = "Welcome to <Placeholer>, please send us the name of your suburb or hit the GEO LOCATION button below"
 
-sql_create_user_table = """CREATE TABLE IF NOT EXISTS tasks (
+sql_create_user_table = """CREATE TABLE IF NOT EXISTS user (
                                     id integer PRIMARY KEY,
                                     user_id integer NOT NULL,
                                     name text NOT NULL,
@@ -86,57 +86,53 @@ def handle(msg):
     Test = msg    
     #print (Test)
 
+    try:
+        if msg["location"]:
+            print ("\n""location found")
+            sqliteinitialinfo = (msg["from"]["id"], msg["from"]["first_name"], msg["location"]["latitude"], msg["location"]["longitude"])
+            print ("saving to sqlite database")
+            conn = create_connection(SqlitePath)
+            create_intitaldb(conn, sqliteinitialinfo)
+            print ("saved to sqlite database")
+            select_all_tasks(conn)
+            conn.close()
+    except KeyError as error:   
+        pass
     
-    
-    if "location" in msg:
-        print ("Location Found")
-        SqliteInitialinfo = (msg["from"]["id"], msg["from"]["first_name"], msg["location"]["latitude"], msg["location"]["longitude"])
-        print ("Saving To Sqlite Database")
-        conn = create_connection(SqlitePath)
-        create_intitaldb(conn, SqliteInitialinfo)
-        print ("Saved To Sqlite Database")
-        select_all_tasks(conn)
-        conn.close()
 
-        
-    
-    if msg['text'] == "/start":
-        bot.sendMessage(chat_id, WelcomeMessage,
-                            reply_markup=ReplyKeyboardMarkup(resize_keyboard = True,
-                                keyboard=[
-                                    [KeyboardButton(text="\U0001f4cd GEO LOCATION \U0001f4cd", request_location=True)]
-                                ]
-                            ))
-        
+    try:
+        if msg["text"] == "/start":
+            bot.sendMessage(chat_id, WelcomeMessage,
+                                reply_markup=ReplyKeyboardMarkup(resize_keyboard = True,
+                                    keyboard=[
+                                        [KeyboardButton(text="\U0001f4cd GEO LOCATION \U0001f4cd", request_location=True)]
+                                    ]
+                                ))
+    except KeyError as error:   
+        pass  
 
 
 
 
 
-
-print ("Setting Up Sqlite Database")
-conn = create_connection(SqlitePath)
+def main():
+    print ("Setting Up Sqlite Database")
+    conn = create_connection(SqlitePath)
             
-if conn is not None:
-        # create user database table
-        create_table(conn, sql_create_user_table)
-        print("Sqlite db Tables Created")
-        conn.close()
-else:
-        print("Error! cannot create the database connection.")
+    if conn is not None:
+            # create user database table
+            create_table(conn, sql_create_user_table)
+            print("Sqlite db Tables Created")
+            conn.close()
+    else:
+            print("Error! cannot create the database connection.")
 
 
 
 
-
+main()
 MessageLoop(bot, handle).run_as_thread()
 
-ReplyKeyboardMarkup(keyboard=[[
-    KeyboardButton(
-        text='Location',
-        request_location=True
-    )
-]])
 
 #Start
 #Welcome Message
