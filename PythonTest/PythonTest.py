f15b63f159a1dc0e91d9f12c4039b99b036eab8d
MCranstoun-Day <MCranstoun-Day@ATCLAP-60>
2019-12-05 14:01:34 +0200

    update WIP

--- PythonTest/PythonTest.py
+++ PythonTest/PythonTest.py
@@ -12,8 +12,9 @@ from datetime import timedelta
 import sqlite3
 from sqlite3 import Error
 
-bot = telepot.Bot('320663225:AAFVzc1y_7dLUu97g1kqbw9PxkQU1aiSUMk')
-SqlitePath = 'D:\pythonsqlite2.db'
+#bot = telepot.Bot('320663225:AAFVzc1y_7dLUu97g1kqbw9PxkQU1aiSUMk') #TelSto
+bot = telepot.Bot('884457382:AAGrGHxVplHPdM5tCT7MfS6cgueeTZKGtP4') #Store1
+SqlitePath = 'C:\TelSto.db'
 
 
 WelcomeMessage = "Welcome to <Placeholer>, please send us the name of your suburb or hit the GEO LOCATION button below"
@@ -36,18 +37,18 @@ def create_connection(db_file):
  
 
  
-def create_userlocationdb(conn, userlocationdb):
+def create_Users(conn, Users):
     """
     Create a new task
     :param conn:
-    :param userlocationdb:
+    :param Users:
     :return:
     """
  
-    sql = ''' INSERT INTO userlocationdb(user_id,name,latitude,longitude)
+    sql = ''' INSERT INTO Users(user_id ,name,latitude,longitude)
               VALUES(?,?,?,?) '''
     cur = conn.cursor()
-    cur.execute(sql, userlocationdb)
+    cur.execute(sql, Users)
     return cur.lastrowid
 
 
@@ -58,7 +59,7 @@ def select_all_tasks(conn):
     :return:
     """
     cur = conn.cursor()
-    cur.execute("SELECT * FROM userlocationdb")
+    cur.execute("SELECT * FROM Users")
  
     rows = cur.fetchall()
  
@@ -66,13 +67,14 @@ def select_all_tasks(conn):
         print(row)
      
 
-def CheckIfUserExistsInDb(id):
+def CheckIfUsersExistsInDb(id):
     try:
         conn = create_connection(SqlitePath)
         cursor = conn.cursor()
-        print("Checking if User Exists On The DB")
+        print("Checking if Users Exists On The DB")
 
-        sql_select_query = """select * from userlocationdb where user_id = ?"""
+
+        sql_select_query = """select * from Users where user_id  = ?"""
         cursor.execute(sql_select_query, (id,))
         records = cursor.fetchone()
         cursor.close()
@@ -89,13 +91,13 @@ def CheckIfUserExistsInDb(id):
             conn.close()
             print("The SQLite connection is closed")
 
-def updateSqliteTable(user_id, name, longitude, latitude):
+def updateSqliteTable(user_id , name, longitude, latitude):
     try:
         conn = create_connection(SqlitePath)
         cursor = conn.cursor()
         print("Connected to SQLite")
 
-        sql_update_query = """Update userlocationdb set name = ?, longitude = ?, latitude = ? where user_id = ?"""
+        sql_update_query = """Update Users set name = "MCD", longitude = '1',latitude = '2'  where user_id  =297725915"""
         cursor.execute(sql_update_query)
         conn.commit()
         print("Record Updated successfully ")
@@ -119,18 +121,18 @@ def handle(msg):
     try:
         if msg["location"]:
             print ("\n""Message with Location Data recieved")
-            if CheckIfUserExistsInDb(msg["from"]["id"])==False:
-                print("user_id not found, creating new Sqlite record...")
+            if CheckIfUsersExistsInDb(msg["from"]["id"])==False:
+                print("user_id  not found, creating new Sqlite record...")
                 sqliteinitialinfo = (msg["from"]["id"], msg["from"]["first_name"], msg["location"]["latitude"], msg["location"]["longitude"])
                 print ("saving to sqlite database")
                 conn = create_connection(SqlitePath)
-                create_userlocationdb(conn, sqliteinitialinfo)
+                create_Users(conn, sqliteinitialinfo)
                 conn.commit()
                 print ("saved to sqlite database")
                 conn = create_connection(SqlitePath)
                 select_all_tasks(conn)
             else:
-                print ("user_id already exists, updating location and name")
+                print ("user_id  already exists, updating location and name")
                 sqliteinitialinfo = (msg["from"]["id"], msg["from"]["first_name"], msg["location"]["latitude"], msg["location"]["longitude"])
                 print ("saving to sqlite database")
                 conn = create_connection(SqlitePath)
@@ -143,7 +145,7 @@ def handle(msg):
         pass
     
     #if msg["text"]:
-    #     User_id = msg["from"]["id"]
+    #     user_id  = msg["from"]["id"]
 
 
 
