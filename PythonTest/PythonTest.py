c39825ebab5d29c0a4b3997d86b1b114199b5b9c
dacelectronicssa@gmail.com <dacelectronicssa@gmail.com>
2019-12-05 23:15:41 +0200

    Delegator Bot is working and its working well! Jus t got it working, not much further tonight.

--- PythonTest/PythonTest.py
+++ PythonTest/PythonTest.py
@@ -1,6 +1,7 @@
 import telepot
 from telepot.loop import MessageLoop
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
+from telepot.delegate import pave_event_space, per_chat_id, create_open
 from pprint import pprint
 #import ConfigParser
 import threading
@@ -13,7 +14,10 @@ import sqlite3
 from sqlite3 import Error
 from geopy.geocoders import Nominatim
 
-bot = telepot.Bot('320663225:AAFVzc1y_7dLUu97g1kqbw9PxkQU1aiSUMk')
+
+TOKEN = '320663225:AAFVzc1y_7dLUu97g1kqbw9PxkQU1aiSUMk'
+
+
 SqlitePath = 'D:\pythonsqlite2.db'
 geolocator = Nominatim(user_agent="Telsto")
 
@@ -93,105 +97,115 @@ def CheckIfUserExistsInDb(id):
 
 
 
-def handle(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    Test = msg    
-    #print (Test)
 
-    try:
-        if msg["location"]:
-            print ("\n""Message with Location Data recieved")
-            if CheckIfUserExistsInDb(msg["from"]["id"])==False:
-                print("user_id not found, creating new Sqlite record...")
-                sqliteinitialinfo = (msg["from"]["id"], msg["from"]["first_name"], msg["location"]["latitude"], msg["location"]["longitude"])
-                conn = create_connection(SqlitePath)
-                create_userlocationdb(conn, sqliteinitialinfo)
-                conn.commit()
-                print ("created new entry in DB")
-                conn = create_connection(SqlitePath)
-                select_all_tasks(conn)
-            else:
-                print ("user_id already exists, updating location and name in DB")
-                conn = create_connection(SqlitePath)
-                cur = conn.cursor()
-                UpdatedName = msg["from"]["first_name"]
-                user_id = msg["from"]["id"]
-                UpdatedsLatitude = msg["location"]["latitude"]
-                UpdatedLongitude = msg["location"]["longitude"]
-                cur.execute('UPDATE userlocationdb SET name = ?, latitude = ?, longitude = ? WHERE user_id = ?', (UpdatedName, UpdatedsLatitude, UpdatedLongitude, user_id))
-                conn.commit()
-                print ("updated DB entry success")
-                conn = create_connection(SqlitePath)
-                select_all_tasks(conn)
+class MessageCounter(telepot.helper.ChatHandler):
+    def __init__(self, *args, **kwargs):
+        super(MessageCounter, self).__init__(*args, **kwargs)
+        self._count = 0
+
+    def on_chat_message(self, msg):
+        self._count += 1
+        #self.sender.sendMessage(self._count)
+        try:
+            if msg["text"] == "/start":
+                self.sender.sendMessage("Hi " + msg["from"]["first_name"] + ",\n\nWelcome to the TelSto, Please send us the name of your suburb or click the Geo Location button below...",
+                                    reply_markup=ReplyKeyboardMarkup(resize_keyboard = True,
+                                        keyboard=[
+                                            [KeyboardButton(text="\U0001f4cd GEO LOCATION \U0001f4cd", request_location=True)]
+                                        ]
+                                    ))
+        except KeyError as error:   
+                pass  
+
+
+
+
+
+
+
+
+
+
+
+#def handle(msg):
+#    content_type, chat_type, chat_id = telepot.glance(msg)
+#    Test = msg    
+#    #print (Test)
+
+#    try:
+#        if msg["location"]:
+#            print ("\n""Message with Location Data recieved")
+#            if CheckIfUserExistsInDb(msg["from"]["id"])==False:
+#                print("user_id not found, creating new Sqlite record...")
+#                sqliteinitialinfo = (msg["from"]["id"], msg["from"]["first_name"], msg["location"]["latitude"], msg["location"]["longitude"])
+#                conn = create_connection(SqlitePath)
+#                create_userlocationdb(conn, sqliteinitialinfo)
+#                conn.commit()
+#                print ("created new entry in DB")
+#                conn = create_connection(SqlitePath)
+#                select_all_tasks(conn)
+#            else:
+#                print ("user_id already exists, updating location and name in DB")
+#                conn = create_connection(SqlitePath)
+#                cur = conn.cursor()
+#                UpdatedName = msg["from"]["first_name"]
+#                user_id = msg["from"]["id"]
+#                UpdatedsLatitude = msg["location"]["latitude"]
+#                UpdatedLongitude = msg["location"]["longitude"]
+#                cur.execute('UPDATE userlocationdb SET name = ?, latitude = ?, longitude = ? WHERE user_id = ?', (UpdatedName, UpdatedsLatitude, UpdatedLongitude, user_id))
+#                conn.commit()
+#                print ("updated DB entry success")
+#                conn = create_connection(SqlitePath)
+#                select_all_tasks(conn)
             
-    except KeyError as error:   
-        pass
+#    except KeyError as error:   
+#        pass
     
-    if msg["text"] != "/start":
-        if CheckIfUserExistsInDb(msg["from"]["id"])==False:
-            print ("Search suburb")
-            location = geolocator.geocode(msg["text"])
-            print(location.address)
-            print((location.latitude, location.longitude))
-            bot.sendMessage(chat_id, location.address + "\n\nIs this correct?",
-                                reply_markup=ReplyKeyboardMarkup(resize_keyboard = True,
-                                    keyboard=[
-                                        [KeyboardButton(text="Yes Sir \U0001f44d")],
-                                        [KeyboardButton(text="Oops, Let me try again \U0001f629" )]
-                                ]
-                            ))
+#    if msg["text"] != "/start":
+#        if CheckIfUserExistsInDb(msg["from"]["id"])==False:
+#            print ("Search suburb")
+#            location = geolocator.geocode(msg["text"])
+#            print(location.address)
+#            print((location.latitude, location.longitude))
+#            bot.sendMessage(chat_id, location.address + "\n\nIs this correct?",
+#                                reply_markup=ReplyKeyboardMarkup(resize_keyboard = True,
+#                                    keyboard=[
+#                                        [KeyboardButton(text="Yes Sir \U0001f44d")],
+#                                        [KeyboardButton(text="Oops, Let me try again \U0001f629" )]
+#                                ]
+#                            ))
 
 
 
-    try:
-        if msg["text"] == "/start":
-            bot.sendMessage(chat_id, "Hi " + msg["from"]["first_name"] + ",\n\nWelcome to the TelSto, Please send us the name of your suburb or click the Geo Location button below...",
-                                reply_markup=ReplyKeyboardMarkup(resize_keyboard = True,
-                                    keyboard=[
-                                        [KeyboardButton(text="\U0001f4cd GEO LOCATION \U0001f4cd", request_location=True)]
-                                    ]
-                                ))
-    except KeyError as error:   
-        pass  
+#    try:
+#        if msg["text"] == "/start":
+#            bot.sendMessage(chat_id, "Hi " + msg["from"]["first_name"] + ",\n\nWelcome to the TelSto, Please send us the name of your suburb or click the Geo Location button below...",
+#                                reply_markup=ReplyKeyboardMarkup(resize_keyboard = True,
+#                                    keyboard=[
+#                                        [KeyboardButton(text="\U0001f4cd GEO LOCATION \U0001f4cd", request_location=True)]
+#                                    ]
+#                                ))
+#    except KeyError as error:   
+#        pass  
 
 
-
-
-
-#class MessageCounter(telepot.helper.ChatHandler):
-#    def __init__(self, *args, **kwargs):
-#        super(MessageCounter, self).__init__(*args, **kwargs)
-#        self._count = 0
-
-#    def on_chat_message(self, msg):
-#        self._count += 1
-#        self.sender.sendMessage(self._count)
-
-#TOKEN = sys.argv[1]  # get token from command-line
-
-#bot = telepot.DelegatorBot(TOKEN, [
-#    pave_event_space()(
-#        per_chat_id(), create_open, MessageCounter, timeout=10),
-#])
-#MessageLoop(bot).run_as_thread()
-
-#while 1:
-#    time.sleep(10)
 
 
 
 
 def main():
     print ("Program Start")
-    #conn = create_connection(SqlitePath)
+
     
-    
-
-
-
 
 main()
-MessageLoop(bot, handle).run_as_thread()
+
+bot = telepot.DelegatorBot(TOKEN, [
+    pave_event_space()(
+        per_chat_id(), create_open, MessageCounter, timeout=10),
+])
+
+MessageLoop(bot).run_as_thread()
 
 
 #Start
